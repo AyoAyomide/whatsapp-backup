fcdad54b2deb2d32198d92a1d197cea5d3810fbf
ayoayomide <ayoayomide124@gmail.com>
2025-02-07 13:26:58 +0100

    indexdb learn

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello world");
+import { openDB, deleteDB, wrap, unwrap } from 'idb';
+
+async function doDatabaseStuff() {
+    const db = await openDB("demo", 1, {
+        upgrade(db) {
+            if (!db.objectStoreNames.contains("notes"))
+                db.createObjectStore("notes", { keyPath: "id" });
+        }
+    });
+
+    const data = await db.getAll("notes");
+
+    console.dir(data);
+
+}
+
+doDatabaseStuff();
+
