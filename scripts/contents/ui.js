1971fbe0feb4f3f8d9b4a7e338c75d7a4f7d82a2
ayoayomide <ayoayomide124@gmail.com>
2025-02-18 00:29:36 +0100

    contact update v2

--- scripts/contents/ui.js
+++ scripts/contents/ui.js
@@ -2,34 +2,50 @@ let { render, signal, component } = reef;
 
 class UI {
     constructor() {
+
         this.contactStore = signal({
-            contactSize: 2,
-            inputValue: ""
+            contactSize: "",
+            inputPrefix: "",
+            inputSuffix: "",
         });
-        this.initializeCounter();
         this.initializeComponent();
+        this.getContactSize();
     }
 
-    initializeCounter() {
-        setInterval(() => {
-            this.contactStore.contactSize++;
-        }, 100);
+    getContactSize() {
+        chrome.storage.local.get("count", (result) => {
+            let value = result.count || 0;
+            this.contactStore.contactSize = value;
+        });
     }
 
     watchEvent() {
-        if (!event.target.matches('[c-input]')) return;
-        this.contactStore.inputValue = event.target.value;
+        let isPrefix = event.target.matches('[input-prefix');
+        let isSuffix = event.target.matches('[input-suffix');
+
+        if (!isPrefix && !isSuffix) return;
+
+        if (isPrefix) {
+            this.contactStore.inputPrefix = event.target.value;
+        } else if (isSuffix) {
+            this.contactStore.inputSuffix = event.target.value;
+        }
     }
 
     template() {
-        let { contactSize, inputValue } = this.contactStore;
+        let { contactSize, inputPrefix, inputSuffix } = this.contactStore;
 
-        return `<div>
-        <p>${contactSize}</p>
-        <input c-input type="text"/>
-        <p>${inputValue}</p>
-        <a href="#">Save contact</a>
-        </div>`;
+        return `
+        <p>Found ${contactSize} contact</p>
+
+        <p class="grouped">
+            <input placeholder="Prefix: Customer" input-prefix type="text"/>
+            <input placeholder="Suffix: 001" input-suffix type="text"/>
+        </p>
+        
+        <p>${inputPrefix} ${inputSuffix}</p>
+        <a href="#" class="button primary">Export</a>
+        `;
     }
 
     initializeComponent() {
